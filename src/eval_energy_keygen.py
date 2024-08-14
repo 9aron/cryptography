5767b162adf8b70af7924998b5ee0b950530961d
lrdtfk <laridtoufik.2022@gmail.com>
2024-08-14 16:47:44 +0100

    fix something in eval file

--- src/eval_energy_keygen.py
+++ src/eval_energy_keygen.py
@@ -14,11 +14,6 @@ from um25c import get_mwh_data, connect_to_usb_tester
 
 csv_file_path = 'csv/keygen_energy.csv' # TODO put this in config files, and make code for it to exec well
 
-# TODO change this when needed
-SEC_LVL = SEC_LVL[3:4]
-RSA_KEYSIZE = RSA_KEYSIZE[3:4]
-EC = EC[3:4]
-
 
 # Measure energy consumption for rsa key generation
 def eval_rsa_keygen(sock, key_size, desc, form=BAR_FORMAT, n_t=N_TEST):
