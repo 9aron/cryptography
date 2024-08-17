6f9833674f689cffae9757f5cd4fc3e52d9c8ec5
lrdtfk <laridtoufik.2022@gmail.com>
2024-08-17 15:08:02 +0100

    add auto eval script

---
+++ eval_energy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd ~/cryptography/src/
+
+python3 eval_energy_keygen.py
+python3 eval_energy_encrypt.py
+python3 eval_energy_decrypt.py
+python3 eval_energy_sign.py
+python3 eval_energy_verify.py
